0aba17a3d04332825df8d3a3505d6559506eaf01
Victor, Jason M <jason_victor@merck.com>
2014-02-04 16:13:45 -0500

    Early commit - not functional yet.

---
+++ Assignment1/GameCardViewController.m
@@ -0,0 +1,40 @@
+//
+//  GameCardViewController.m
+//  Assignment1
+//
+//  Created by Victor, Jason M on 2/4/14.
+//  Copyright (c) 2014 Victor, Jason M. All rights reserved.
+//
+
+#import "GameCardViewController.h"
+
+@interface GameCardViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+
+@end
+
+@implementation GameCardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
